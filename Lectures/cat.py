2908352b763a7de639925485fadc469250642395
ivan-dgz <ivan.dominguez@outlook.com>
2023-08-30 18:16:45 -0700

    partial cat file

---
+++ Lectures/cat.py
@@ -0,0 +1,6 @@
+### Make 3 meows
+
+i = 3 # defined a variable that indicates the amount of times we want the action to take place
+while i != 0: # a condition that the program must meet in order to execute the action, otherwise stopping.
+    print("meow") # the action we want to take
+    i = i - 1 # a change or update to the variable that happens in every loop to prevent from infinite looping.
